9ac971018f8b3065024d83e28c3809f15498ce83
chagy <chagy_x_@hotmail.com>
2021-05-20 21:29:14 +0700

    14 Defining relationships among models

--- app/Models/User.js
+++ app/Models/User.js
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/framework/src/Hash')} */
-const Hash = use('Hash')
+const Hash = use("Hash");
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
 class User extends Model {
   // static get table(){
@@ -15,18 +15,18 @@ class User extends Model {
   //   return 'username'
   // }
 
-  static boot () {
-    super.boot()
+  static boot() {
+    super.boot();
 
     /**
      * A hook to hash the user password before saving
      * it to the database.
      */
-    this.addHook('beforeSave', async (userInstance) => {
+    this.addHook("beforeSave", async (userInstance) => {
       if (userInstance.dirty.password) {
-        userInstance.password = await Hash.make(userInstance.password)
+        userInstance.password = await Hash.make(userInstance.password);
       }
-    })
+    });
   }
 
   /**
@@ -39,9 +39,19 @@ class User extends Model {
    *
    * @return {Object}
    */
-  tokens () {
-    return this.hasMany('App/Models/Token')
+  tokens() {
+    return this.hasMany("App/Models/Token");
+  }
+
+  podcast() {
+    return this.hasOne("App/Models/Podcast");
+  }
+
+  subscriptions() {
+    return this.belongsToMany("App/Models/Podcast")
+      .pivotTable("subscriptions")
+      .withTimestamps();
   }
 }
 
-module.exports = User
+module.exports = User;
